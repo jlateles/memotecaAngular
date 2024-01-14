27eaf52095259ff17199d4a43eb5e9213f86979d
jlateles <juliatelesicm@gmail.com>
2024-01-13 21:32:00 -0300

    Primeiro commit

---
+++ src/app/componentes/pensamentos/excluir-pensamento/excluir-pensamento.component.ts
@@ -0,0 +1,45 @@
+import { Pensamento } from './../pensamento';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { PensamentoService } from '../pensamento.service';
+
+@Component({
+  selector: 'app-excluir-pensamento',
+  templateUrl: './excluir-pensamento.component.html',
+  styleUrl: './excluir-pensamento.component.css'
+})
+export class ExcluirPensamentoComponent implements OnInit {
+
+  pensamento: Pensamento= {
+    id: 0,
+    conteudo: '',
+    autoria: '',
+    modelo: ''
+  }
+
+  constructor(private service: PensamentoService,
+    private Router: Router,
+    private route: ActivatedRoute
+    ){}
+
+  ngOnInit(): void {
+     const id = this.route.snapshot.paramMap.get('id')
+     this.service.buscarPorID(parseInt(id!)).subscribe((pensamento) => {
+      this.pensamento = pensamento
+     })
+  }
+
+  excluirPensamento(){
+    if(this.pensamento.id){
+      this.service.excluir(this.pensamento.id).subscribe(() =>{
+        this.Router.navigate(['/listarPensamento'])
+      })
+    }
+  }
+
+  cancelar(){
+    this.Router.navigate(['/listarPensamento'])
+  }
+
+
+} // final
---
+++ src/app/componentes/pensamentos/pensamento/pensamento.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Pensamento } from '../pensamento';
+
+@Component({
+  selector: 'app-pensamento',
+  templateUrl: './pensamento.component.html',
+  styleUrl: './pensamento.component.css'
+})
+export class PensamentoComponent implements OnInit {
+
+  @Input() pensamento: Pensamento = {
+    id: 0,
+    conteudo: 'O pouco que sei não dá para compreender a vida, então a explicação está no que desconheço e que tenho a esperança de poder vir a conhecer um pouco mais',
+    autoria: 'Clarice Lispector',
+    modelo: 'modelo3'
+  }
+
+  constructor(){}
+
+  ngOnInit(): void {
+
+  }
+
+  larguraPensamento(): string {
+    if(this.pensamento.conteudo.length >= 256){
+      return 'pensamento-g'
+    }
+    return 'pensamento-p'
+  }
+}
