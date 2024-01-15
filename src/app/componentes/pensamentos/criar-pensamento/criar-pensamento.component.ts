b0797d856d246221cd94c325ab56e420393b9968
jlateles <juliatelesicm@gmail.com>
2024-01-15 15:39:02 -0300

    commit alterações

--- src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Pensamento } from '../pensamento';
 import { PensamentoService } from '../pensamento.service';
 import { Router } from '@angular/router';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-criar-pensamento',
@@ -16,13 +17,21 @@ export class CriarPensamentoComponent implements OnInit {
     modelo: 'modelo1'
   }
 
-  constructor(private service: PensamentoService,
-  private router: Router
-  ){}
+  formulario!: FormGroup;
+
+  constructor(
+    private service: PensamentoService,
+    private router: Router,
+    private formBuilder: FormBuilder
+  ){ }
 
 
   ngOnInit(): void {
-
+      this.formulario = this.formBuilder.group({
+        conteudo: ['formulário reativo'],
+        autoria: [''],
+        modelo: ['modelo1']
+      })
   }
 
   criarPensamento(){
