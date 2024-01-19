50ede0b8ad46cd18f249992b674df22eaef582da
jlateles <juliatelesicm@gmail.com>
2024-01-18 21:56:11 -0300

    commit consertandoBugs2

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Pensamento } from '../pensamento';
 import { PensamentoService } from '../pensamento.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-listar-pensamento',
@@ -12,18 +13,20 @@ export class ListarPensamentoComponent implements OnInit {
   listaPensamentos: Pensamento[] = [];
   paginaAtual: number = 1;
   haMaisPensamentos: boolean = true;
-  filtro: string = ''
+  filtro: string = '';
+  favoritos: boolean = false
 
-  constructor(private service: PensamentoService){}
+  constructor(private service: PensamentoService,
+    private router: Router){}
 
   ngOnInit(): void {
-    this.service.listar(this.paginaAtual, this.filtro).subscribe((listaPensamentos) => {
+    this.service.listar(this.paginaAtual, this.filtro, this.favoritos).subscribe((listaPensamentos) => {
       this.listaPensamentos = listaPensamentos
     })
   }
 
   carregarMaisPensamentos(){
-    this.service.listar(++this.paginaAtual, this.filtro)
+    this.service.listar(++this.paginaAtual, this.filtro, this.favoritos)
     .subscribe(listaPensamentos => {
       this.listaPensamentos.push(...listaPensamentos);
       if(!listaPensamentos.length){
@@ -36,21 +39,25 @@ export class ListarPensamentoComponent implements OnInit {
 
       this.haMaisPensamentos = true;
       this.paginaAtual = 1;
-      this.service.listar(this.paginaAtual, this.filtro)
+      this.service.listar(this.paginaAtual, this.filtro, this.favoritos)
         .subscribe(listaPensamentos => {
           this.listaPensamentos = listaPensamentos;
         })
     }
 
   listarFavoritos() {
+    this.favoritos = true;
     this.haMaisPensamentos = true;
     this.paginaAtual = 1;
-    this.service.listarPensamentosFavoritos(this.paginaAtual, this.filtro)
-    .subscribe(listarPensamentosFavoritos => {
-      this.listaPensamentos = listarPensamentosFavoritos;
+    this.service.listar(this.paginaAtual, this.filtro, this.favoritos)
+    .subscribe(listaPensamentosFavoritos => {
+      this.listaPensamentos = listaPensamentosFavoritos;
     })
   }
 
+  voltarAoMural(){
+    this.router.navigate(['/listarPensamento'])
+  }
 } // final
 
 
--- src/app/componentes/pensamentos/pensamento.service.ts
+++ src/app/componentes/pensamentos/pensamento.service.ts
@@ -12,7 +12,7 @@ export class PensamentoService {
 
   constructor(private http: HttpClient) { }
 
-  listar(pagina: number, filtro: string): Observable<Pensamento[]> {
+  listar(pagina: number, filtro: string, favoritos: boolean): Observable<Pensamento[]> {
 
     const itensPorPagina = 5;
     let params = new HttpParams()
@@ -22,25 +22,14 @@ export class PensamentoService {
     if(filtro.trim().length  > 2 ){
       params = params.set("q", filtro)
     }
+
+    if(favoritos){
+      params = params.set("favorito", true)
+    }
+
 
    // return this.http.get<Pensamento[]>( `${this.API}?_page=${pagina}&_limit=${itensPorPagina}`)  / não é uma boa prática
     return this.http.get<Pensamento[]>(this.API,{ params }) // não funcionou com a classe httpParams
-  }
-
-  listarPensamentosFavoritos(pagina: number, filtro: string): Observable<Pensamento[]> {
-
-    const itensPorPagina = 5;
-    let params = new HttpParams()
-    .set("_page", pagina)
-    .set("_limit", itensPorPagina)
-    .set("favorito", true)
-
-    if(filtro.trim().length  > 2 ){
-      params = params.set("q", filtro)
-    }
-
-    return this.http.get<Pensamento[]>(this.API,{ params })
-
   }
 
   criar(pensamento: Pensamento): Observable<Pensamento>{
